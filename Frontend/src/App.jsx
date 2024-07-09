1878f3109b8d417e873651ed6387ccafd40a5e24
harshraj1512 <harshprogram63@gmail.com>
2024-07-09 22:40:57 +0530

    theme implemented

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import Home from './home/Home'
 import Courses from './courses/Courses'
 
 
 function App() {
   return (<>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/course" element={<Courses />} />
-  </Routes>
+    <div className="dark:bg-slate-900 dark:text-white">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course" element={<Courses />} />
+      </Routes>
+    </div>
   </>
   )
 }
