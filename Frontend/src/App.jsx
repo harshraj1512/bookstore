ec069b84177391057024f4b83ac885e26433790e
harshraj1512 <harshprogram63@gmail.com>
2024-07-10 18:16:19 +0530

    login signup frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from './home/Home'
 import Courses from './courses/Courses'
+import Signup from './components/Signup'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course" element={<Courses />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   </>
