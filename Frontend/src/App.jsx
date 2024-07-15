a448afabea75672dc71cba5d676cbca535a459b8
harshraj1512 <harshprogram63@gmail.com>
2024-07-15 12:47:30 +0530

    backend data fetch, user post and data display on frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from './home/Home'
 import Courses from './courses/Courses'
 import Signup from './components/Signup'
+import Contacts from './components/contact/Contacts'
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/course" element={<Courses />} />
+        <Route path="/contact" element={<Contacts />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
---
+++ Frontend/src/components/contact/Contacts.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../Navbar'
+import Footer from '../Footer'
+import Contact from '../Contact'
+
+function Contacts() {
+  return (
+    <>
+    <Navbar />
+    <div>
+        <Contact />
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default Contacts
